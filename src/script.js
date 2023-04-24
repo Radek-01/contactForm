0a7752d0c9d8093c98eb587c1ef65bd3a1484e32
Radek-01 <rlasiecki@gmail.com>
2023-04-24 16:12:13 +0200

    as

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
 "use strict";
-"use strict";
 
 function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 }
-
-console.log(validateEmail("dlasiecki@gmail.com"));
