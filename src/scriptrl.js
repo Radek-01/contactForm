d3ea38e13eba4393e37041eabccf2ed6d0ad5843
David <dlasiecki@gmail.com>
2023-04-24 20:55:30 +0200

    form data

--- src/scriptrl.js
+++ src/scriptrl.js
@@ -1,39 +1,39 @@
-function appendRowToTable(data) {
-  const body = document.querySelector("body");
-  let table = document.querySelector("table");
+// function appendRowToTable(data) {
+//   const body = document.querySelector("body");
+//   let table = document.querySelector("table");
 
-  if (!table) {
-    table = document.createElement("table");
-    body.append(table);
-  }
-  //prepare table header
-  if (!table.innerHTML) {
-    const tblHeadEl = document.createElement("thead");
-    const tblRowEl = document.createElement("tr");
+//   if (!table) {
+//     table = document.createElement("table");
+//     body.append(table);
+//   }
+//   //prepare table header
+//   if (!table.innerHTML) {
+//     const tblHeadEl = document.createElement("thead");
+//     const tblRowEl = document.createElement("tr");
 
-    const thNameEl = document.createElement("th");
-    thNameEl.textContent = "Name";
+//     const thNameEl = document.createElement("th");
+//     thNameEl.textContent = "Name";
 
-    const thSurnameEl = document.createElement("th");
-    thSurnameEl.textContent = "Surname";
+//     const thSurnameEl = document.createElement("th");
+//     thSurnameEl.textContent = "Surname";
 
-    const thEmailEl = document.createElement("th");
-    thEmailEl.textContent = "Email";
+//     const thEmailEl = document.createElement("th");
+//     thEmailEl.textContent = "Email";
 
-    const thMessageEl = document.createElement("th");
-    thMessageEl.textContent = "Message";
+//     const thMessageEl = document.createElement("th");
+//     thMessageEl.textContent = "Message";
 
-    tblRowEl.append(thNameEl, thSurnameEl, thEmailEl, thMessageEl);
-    tblHeadEl.append(tblRowEl);
-    table.append(tblHeadEl);
-  }
+//     tblRowEl.append(thNameEl, thSurnameEl, thEmailEl, thMessageEl);
+//     tblHeadEl.append(tblRowEl);
+//     table.append(tblHeadEl);
+//   }
 
-  const { name, surname, email, message } = data;
+//   const { name, surname, email, message } = data;
 
-  // const addNewRow()
+//   // const addNewRow()
 
-  //   const trEl = document.createElement('tr');
-  //   const tdEl = document.createElement('td');
-}
+//   //   const trEl = document.createElement('tr');
+//   //   const tdEl = document.createElement('td');
+// }
 
-console.log(appendRowToTable({}));
+// console.log(appendRowToTable({}));
