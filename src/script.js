8dd78018bf6e81040737d22916a254842e7e2a41
Radek-01 <rlasiecki@gmail.com>
2023-04-24 18:02:49 +0200

    table

--- src/script.js
+++ src/script.js
@@ -35,12 +35,34 @@ function appendRowToTable(data) {
     table.append(tblHeadEl);
   }
 
-  const { name, surname, email, message } = data;
+  //   const { name, surname, email, message } = data;
 
-  //   const addNewRow()
+  const addNewRow = ({ name, surname, email, message }) => {
+    // const {name, surname, email, message} = data;
+    const trow = document.createElement("tr");
+    const tdName = document.createElement("td");
+    tdName.textContent = name;
+    const tdSurname = document.createElement("td");
+    tdSurname.textContent = surname;
+    const tdEmail = document.createElement("td");
+    tdEmail.textContent = email;
+    const tdMessage = document.createElement("td");
+    tdMessage.textContent = message;
+
+    trow.append(tdName, tdSurname, tdEmail, tdMessage);
+    return trow;
+  };
+  table.append(addNewRow(data));
 
   //   const trEl = document.createElement('tr');
   //   const tdEl = document.createElement('td');
 }
 
-console.log(appendRowToTable({}));
+console.log(
+  appendRowToTable({
+    name: "Rad",
+    surname: "Las",
+    email: "rlasiecki@gmail.com",
+    message: "Msg",
+  })
+);
