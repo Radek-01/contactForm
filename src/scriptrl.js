82bf16559204a987b15bee1cd2f7b84863705d88
Radek-01 <rlasiecki@gmail.com>
2023-04-24 20:03:08 +0200

    v

--- src/scriptrl.js
+++ src/scriptrl.js
@@ -28,12 +28,42 @@ function appendRowToTable(data) {
     table.append(tblHeadEl);
   }
 
-  const { name, surname, email, message } = data;
+  //   const { name, surname, email, message } = data;
+  //comment
+  const addNewRow = ({ name, surname, email, message }) => {
+    // const {name, surname, email, message} = data;
+    const trow = document.createElement("tr");
+    const tdName = document.createElement("td");
+    tdName.textContent = name;
+    const tdSurname = document.createElement("td");
+    tdSurname.textContent = surname;
+    const tdEmail = document.createElement("td");
+    tdEmail.textContent = email;
+    const tdMessage = document.createElement("td");
+    tdMessage.textContent = message;
 
-  // const addNewRow()
+    trow.append(tdName, tdSurname, tdEmail, tdMessage);
+    return trow;
+  };
+  table.append(addNewRow(data));
 
   //   const trEl = document.createElement('tr');
   //   const tdEl = document.createElement('td');
 }
 
-console.log(appendRowToTable({}));
+console.log(
+  appendRowToTable({
+    name: "Rad",
+    surname: "Las",
+    email: "rlasiecki@gmail.com",
+    message: "Msg",
+  })
+);
+console.log(
+  appendRowToTable({
+    name: "Daw",
+    surname: "Las",
+    email: "dlasiecki@gmail.com",
+    message: "Msg1",
+  })
+);
